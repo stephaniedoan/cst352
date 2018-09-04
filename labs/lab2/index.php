c24476ef29806fcb91b60881f9a0025b9235c08b
stephaniedoan <sdoan@csumb.edu>
2018-09-04 20:26:31 +0000

    Completed Lab 1

---
+++ labs/lab2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        <?php
+        $symbol = "grapes";
+        
+        echo "<img src='img/$symbol.png' alt='Seven' title='Seven' />";
+        
+        ?>
+        
+        
+    </body>
+</html>
