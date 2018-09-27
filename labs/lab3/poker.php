90d7eb9bb3c7b97b77e8df1b0b11e8b67f7fa729
stephaniedoan <sdoan@csumb.edu>
2018-09-27 01:16:19 +0000

    Lab 3

--- labs/lab3/poker.php
+++ labs/lab3/poker.php
@@ -1,69 +1,109 @@
 <?php
+ $deck = range(1,52);   // $deck = array(1,2,3,4,5,6,7... 52);
+ shuffle($deck);
+ //print_r($deck);
+ $suit = array("clubs", "diamonds", "hearts","spades");
+ $totalPoints = 0;
 
-    $deck = range(1,52);
-    shuffle($deck);
+ function hand(){
+    global $deck, $suit, $totalPoints;
+    $aceCounter = 0;
+    $pointCounter = 0;
     
-   
-
-function hand() {
-    global $deck;
     
-     for ($i=1; $i<6; $i++) {
-       $lastCard = array_pop($deck);
-       $faceValue = $lastCard % 13;
-    //   echo $faceValue . "-";
-    //   echo $lastCard . " ";
-    
-    if($faceValue==0) {
-        $faceValue = 13;
+    //$handPoints = 0;
+    for($i = 1; $i < 6;$i++) {
+        $lastCard = array_pop($deck);
+        $faceValue = $lastCard % 13;
+        //echo $faceValue . "-";
+        
+        //echo $lastCard . " ";
+        if($faceValue==0){
+            $faceValue = 13;
+        }
+        $indexSuit=($lastCard-1)/13;
+        
+        // Another way to display the right suit
+        // if ($lastCard < 14) {
+        //     $suit = "clubs";
+        // } else if ( $lastCard > 13 && $lastCard < 27 ) {
+        //     $suit = "diamonds";
+        // }
+        $pointCounter =  $pointCounter + $faceValue;  // $pointCounter += $faceValue;
+        if($faceValue==1){
+            echo "<img src='cards/$suit[$indexSuit]/$faceValue.png' alt='$faceValue' title= '$faceValue' class='ace' />";
+            $aceCounter++;  //$aceCounter = $aceCounter + 1; 
+        }else{
+           echo "<img src='cards/$suit[$indexSuit]/$faceValue.png' alt='$faceValue' title= '$faceValue'/>";
+        } 
     }
+    echo $pointCounter;
     
-    $suit = array("clubs","diamonds","hearts","spades");
+    $totalPoints += $pointCounter;
     
-    echo "<img src='cards/clubs/$faceValue.png' alt='$faceValue' title= '$faceValue' />";
+    return $aceCounter;
+ }
+ 
+ function displayCard(){
+     $suit = array("clubs","spades", "diamonds", "hearts");
+     
+     for($i=1; $i<6; $i++){
+       $card = rand(1,13);
+       echo "<img src ='cards/".$suit[rand(0,3)]."/$card.png' alt ='$card' title ='$card' />";
     }
-}
-
-function displayCard() {
-    $suit = array("clubs","diamonds","hearts","spades");
+ }
+ 
+ function displayWinner($p1, $p2) {
+     global $totalPoints;
+    // echo "<h1>";
+     if($p1 > $p2) { 
+         echo "Player 1 wins $totalPoints Points";
+     }
+     else if($p1 < $p2) {
+         echo "Player 2 wins $totalPoints Points";
+     }
+     else {
+         echo "Tie game";
+     }
+    // echo "</h1>";
     
-    for ($i=1; $i<6; $i++) {
-        $card = rand(1,13);
-    
-    echo "<img src='cards/".$suit[rand(0,3)]."/$card.png' alt='$card' title='$card' />";    
-    }
-}
-
+ }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Lab 3: Ace Poker</title>
-        <style type="text/css">
+        <title> Lab 3: Ace Poker </title>
+        <style>
             h1, h2, body {
                 text-align: center;
+            }
+            .ace {
+                border: 2px yellow solid;
             }
         </style>
     </head>
     <body>
-<h1>Ace Poker</h1>
-<h2>Player with the more aces wins all points. </h2>
-<?php
-
-echo "Player 1: ";
-hand();
-
-    echo "<br>";
-
-echo "Player 2: ";
-hand();
-
-?>
-
+        <h1>Ace Poker</h1>
+        <h2>Player with more aces wins all points.</h2>
+        <?php
+        
+        echo "Player 1: ";
+        $p1 = hand();
+        //echo $p1;
+           
+         echo "<br/>";
+        
+        echo "Player 2: ";
+        $p2 = hand();
+        //echo $p2;
+       
+        ?>
+        
+        
+        <h2>
+            <?=displayWinner($p1, $p2)?>
+        </h2>
+        
     </body>
-    <footer>
-        <hr>
-        <p>&copy; 2018. Copyright by Stephanie Doan</p>
-    </footer>
 </html>
