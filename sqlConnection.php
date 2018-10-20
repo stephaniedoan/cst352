d96759e03cd6e0a5ecdaf3950b55ae7fb6f9dd73
stephaniedoan <sdoan@csumb.edu>
2018-10-20 21:25:57 +0000

    Lab5

--- sqlConnection.php
+++ sqlConnection.php
@@ -6,6 +6,15 @@ function getConnection($dbname) {
     $username = "root";
     $password = "";
     
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    }
+    
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
