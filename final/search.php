ba3f3cdf0d85d3369063de6b317b4091ebc4f862
stephaniedoan <sdoan@csumb.edu>
2018-12-13 18:34:18 +0000

    final

--- final/search.php
+++ final/search.php
@@ -88,7 +88,8 @@
 <html>
     <head>
     <title>Search</title>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+         <link href="https://fonts.googleapis.com/css?family=Acme|Work+Sans" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
       	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
       	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script> 
@@ -199,7 +200,7 @@
             type: "GET",
             url: "api/getHeroInfo.php",
             dataType: "json",
-            data: { "hero_id": $(this).attr("id") },
+            data: { "id": $(this).attr("id") },
             success: function(data,status) {
                 //alert(data.id);
                 console.log(data);
